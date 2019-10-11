d7947c7455edabb96f40d0b4db96303e8dab1e20
Romario Gustave <romagustave10@gmail.com>
2019-10-11 19:09:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -23,20 +23,10 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+ 	
+       expected: {:known_for=>"COBOL", :languages=>["COBOL", "FORTRAN"]}
+            got: {:alan_kay=>{:known_for=>"Object Orientation", :languages=>["Smalltalk", "LISP"]}, :dennis_ritchie=>{...>"Unix", :languages=>["C"]}, :grace_hopper=>{:known_for=>"COBOL", :languages=>["COBOL", "FORTRAN"]}}
+
 end
 
 def dennis_ritchies_language
